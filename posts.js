db3bc24ff0c4ed39dd5ddf73f651ff271d7957dc
Leonard Klinger <leonardklinger9@gmail.com>
2026-08-15 22:21:44 +0200

    Nachrichten 1.2.1.1

--- posts.js
+++ posts.js
@@ -42,7 +42,7 @@ const posts = [
     important: true,
     content: `
       <p>Durch die Umstellung auf die Regionalbahn wird die traditionelle Stadtbahnverbindung im oberen Murgtal verschwinden.</p>
-      <p>Durch die von DB Regio gewonnen Ausschreibung wird vorgesehen, dass zur Eröffnung des Rastatter Tunnels der letzte Teil der Ausschreibung
+      <p>Durch die von DB Regio gewonnene Ausschreibung wird vorgesehen, dass zur Eröffnung des Rastatter Tunnels der letzte Teil der Ausschreibung
       vollständig inkraft tritt. Das bedeutet, dass die Stadtbahnverbindung im Murgtal nicht mehr existieren wird. Die Stadtahnen der S8 wenden 
       bereits in Forbach und werden zwischen Forbach und Freudenstadt durch den bisherien Regionalexpress ersetzt, welcher an allen Zwischenstationen halten soll. 
       Wie genau der Fahrplan aussieht, zeigt sich kurz vor dem Fahrplanwechsel Anfang Dezember.</p>
